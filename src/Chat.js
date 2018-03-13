dde62d2725cf3a753dbffe3c11e8ea5de38cbe99
Nathan219 <Nathan219@gmail.com>
2018-03-12 21:04:58 -0700

    Add comments

--- src/Chat.js
+++ src/Chat.js
@@ -2,7 +2,7 @@ import React from "react";
 import io from "socket.io-client";
 import moment from "moment";
 
-const SERVER_URL = process.env.REACT_APP_SERVER_UR || 'localhost:5000';
+const SERVER_URL = process.env.REACT_APP_SERVER_URL || 'localhost:5000';
 console.log(SERVER_URL);
 
 class Chat extends React.Component {
@@ -33,18 +33,38 @@ class Chat extends React.Component {
       this.setServerState(true);
     });
 
+    /**
+     * Adds multiple messages to the beginning of the list.  Used when loading missed messages
+     *
+     * @param {Object[]} messages
+     * @param {Object} messages.author
+     * @param {Object} messages.created
+     * @param {Object} messages.message
+     */
     const addMessages = messages => {
-      messages = messages.filter(message => this.checkMessageExists(message));
+      messages = messages.filter(message => this.isUniqueMessage(message));
       this.setState({messages: [...messages, ...this.state.messages]});
     };
 
+    /**
+     * Adds a single message to the list
+     *
+     * @param {Object} message
+     * @param {Object} message.author
+     * @param {Object} message.created
+     * @param {Object} message.message
+     */
     const addMessage = message => {
-      if (!this.checkMessageExists(message)) {
+      if (!this.isUniqueMessage(message)) {
         return;
       }
       this.setState({messages: [...this.state.messages, message]});
     };
 
+    /**
+     * Send the current message to the socket server
+     * @param ev
+     */
     this.sendMessage = (ev) => {
       ev.preventDefault();
       socket.emit('SEND_MESSAGE', {
@@ -55,18 +75,28 @@ class Chat extends React.Component {
     };
   }
 
-  checkMessageExists (message) {
+  /**
+   * Checks the store of messages to see if the one we're trying to add has already been added.
+   * If it hasn't been added yet, it's added to it.  Good for use in an Array.filter function
+   *
+   * @param {Object} message
+   * @param {Object} message.author
+   * @param {Object} message.created
+   * @param {Object} message.message
+   *
+   * @returns {Boolean} True if the message didn't exist in the store, false if it did.
+   */
+  isUniqueMessage (message) {
     const key = stringifyMessage(message);
     if (this.messageStore[key]) {
       // We've already been given this message
-      return;
+      return false;
     }
     this.messageStore[key] = true;
     return true;
   }
 
   setServerState (serverState) {
-    console.log(serverState);
     if (!serverState) {
       return this.setState({ serverStatus: 'sphere red' });
     }
